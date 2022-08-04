170271eba7c5b3c2b679124c88d8b4489b19c90f
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 03:31:22 -0300

    feat: style Post

--- teste-curso-educa-front/src/pages/Post.js
+++ teste-curso-educa-front/src/pages/Post.js
@@ -45,49 +45,51 @@ function Post () {
   
   
   return(
-    <div className="flex items-center flex-col">
+    <>
       <Header/>
-      <div className="mb-16">
-        <div className="h-[100px] w-screen flex items-center justify-center border-4 border-blue-800">
-          <h1 className="text-4xl text-blue-800"> Educachat </h1>
+      <div className="flex items-center flex-col">
+        <div className="mb-16">
+          <div className="min-h-[25px] mt-8 w-screen flex items-center justify-center">
+            <h1 className="text-4xl font-black text-blue-800"> EducaChat </h1>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center content-center">
-        <div className="h-[500px] w-[650px] overflow-auto border-2">
-          {listMessage && listMessage !== [] && listMessage.map((value) => {
-            return <PostCard 
-              key={value.id}
-              listMessage={listMessage}
-              setListMessage={setListMessage}
-              message={value.message}
-              userName={'usuario'}
-              id={value.id}
-            ></PostCard>
-          })}
-        </div>
-        <form className="flex-1 w-[650px] flex flex-row border-2 border-blue-800">
-          <textarea
-          className="bg-slate-200 w-8/12 text-left"
-          type="message"
-          value={ message }
-          placeholder="faça sua postagem"
-          onChange={({ target: { value } }) => setMessage(value)}
-          >
+        <div className="flex flex-col items-center content-center">
+          <div className="h-[500px] w-[650px] overflow-auto border-2">
+            {listMessage && listMessage !== [] && listMessage.map((value) => {
+              return <PostCard 
+                key={value.id}
+                listMessage={listMessage}
+                setListMessage={setListMessage}
+                message={value.message}
+                userName={'usuario'}
+                id={value.id}
+              ></PostCard>
+            })}
+          </div>
+          <form className="flex-1 w-[650px] flex flex-row border-2 border-blue-800">
+            <textarea
+            className="bg-slate-200 w-8/12 text-left"
+            type="message"
+            value={ message }
+            placeholder="faça sua postagem"
+            onChange={({ target: { value } }) => setMessage(value)}
+            >
 
-          </textarea>
-          <button
-          className="bg-slate-200 w-4/12 hover:bg-slate-400 p-1.5 rounded"
-          type=""
-          onClick={ (e) => {
-            e.preventDefault(); 
-            handleClick() 
-          }}
-          >
-            Clique pra enviar mensagem
-          </button>
-        </form>
+            </textarea>
+            <button
+            className="bg-slate-200 w-4/12 hover:bg-slate-400 p-1.5 rounded"
+            type=""
+            onClick={ (e) => {
+              e.preventDefault(); 
+              handleClick() 
+            }}
+            >
+              Clique pra enviar mensagem
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
