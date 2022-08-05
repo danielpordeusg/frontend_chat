0bcc3e3a418698e551f85daf0270c9e2bb9440fe
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 21:20:09 -0300

    fix: retira o provider

--- teste-curso-educa-front/src/App.js
+++ teste-curso-educa-front/src/App.js
@@ -3,19 +3,17 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import User from './pages/User';
 import Login from './pages/Login';
 import Post from './pages/Post';
-import AppProvider from './context/AppProvider';
+// import AppProvider from './context/AppProvider';
 
 function App() {
   return (
-    <AppProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/post" element={ < Post />}/>
-          <Route path="/login" element={ < Login />}/>
-          <Route path="/" element={ < User />}/>
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/post" element={ < Post />}/>
+        <Route path="/login" element={ < Login />}/>
+        <Route path="/" element={ < User />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
