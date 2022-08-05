0350241281a17344b3335658948bd525098d5953
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-05 00:02:15 -0300

    feat: acrescanta o react-icons e estiliza

--- teste-curso-educa-front/src/components.js/PostCard.js
+++ teste-curso-educa-front/src/components.js/PostCard.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { FaEdit, FaRemoveFormat } from 'react-icons/fa';
 
 function PostCard (props) {
   return(
     <div className="flex flex-row items-center content-center text-center">
       <span className="text-xl font-bold mr-2 text-blue-800">{props.userName}:</span>
       <span className="post-card text-lg">{props.message}</span>
-      <button
-        onClick={ () => {props.handleEdit(props.id)} }
-      >
-        Edit
-      </button>
-      <button
-        onClick={ () => {props.handleRemove(props.id)} }
-      >
-       Remove
-      </button>
-
+      <div className="items-center ">
+        <button
+          className=" min-h-[px] p-2 text-black right-6 items-center justify-center"
+          onClick={ () => {props.handleEdit(props.id)} }
+        >
+          <FaEdit/>
+        </button>
+        <button
+          className=" min-h-[25px] p-2 text-black right-6 items-center"
+          onClick={ () => {props.handleRemove(props.id)} }
+        >
+        <FaRemoveFormat/>
+        </button>
+      </div>
     </div>
   )
 }
