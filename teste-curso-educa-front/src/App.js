2acc85b9c833267eed034a1a59b161a58222ff60
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 21:20:22 -0300

    fix: retira o provider

--- teste-curso-educa-front/src/App.js
+++ teste-curso-educa-front/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import User from './pages/User';
 import Login from './pages/Login';
 import Post from './pages/Post';
-// import AppProvider from './context/AppProvider';
 
 function App() {
   return (
